62685a70bec823b5cba06ddf258360027ecbbdef
sathushnanayakkara <sathufit@gmail.com>
2025-05-04 19:38:22 +0530

    Move server.js to backend and fix deployment

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const API_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5001"
-    : "";
+    : "https://frontyardcricket.onrender.com"; // ✅ your deployed backend URL
 
 export default API_URL;
