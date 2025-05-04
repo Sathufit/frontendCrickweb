507faed6a6152a2c8bc0b46d35190c53f5d623c4
sathushnanayakkara <sathufit@gmail.com>
2025-05-04 18:31:42 +0530

    Feature: added new section

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,10 +1,6 @@
 const API_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5001"
-    : process.env.REACT_APP_API_URL;
-
-if (!API_URL) {
-  throw new Error("❌ API_URL is not defined in environment variables");
-}
+    : ""; // use relative path in production
 
 export default API_URL;
