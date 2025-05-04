4510f1175928263cf1bdb3013c87a887c6131fe7
sathushnanayakkara <sathufit@gmail.com>
2025-05-04 17:10:27 +0530

    Test commit for server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
+// Test commit
+
 
 const app = express();
 app.use(express.json());
