35fd06fbf61565b950e44205286fd564e3178b12
Sathush Nanyakkara <sathufit@gmail.com>
2025-08-26 15:41:33 +0530

    New

--- frontend/src/pages/PlayerStats.js
+++ frontend/src/pages/PlayerStats.js
@@ -22,9 +22,9 @@ const PlayerStats = () => {
       { name: "Chanuka de Silva", fifties: "11" },
       { name: "Achala Shashvika", fifties: "8" },
       { name: "Dulshan Thanoj", fifties: "8" },
-      { name: "Shanaka", fifties: "6" },
+      { name: "Dinal Chamith", fifties: "6" },
       { name: "Savindu Weerarathna", fifties: "4" },
-      { name: "Nidula Lokuge", fifties: "2" },
+      { name: "Nidula Hansaja", fifties: "2" },
       { name: "Farhan Navufal", fifties: "1" },
       { name: "Dihindu Nimsath", fifties: "1" },
       { name: "Ravindu Nanayakkara", fifties: "1" }
